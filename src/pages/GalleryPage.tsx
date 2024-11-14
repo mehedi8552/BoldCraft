c9f37c2c3867cfde99de250d15ea54df74673e95
MD. Mehedi hasan <95436440+mehedi8552@users.noreply.github.com>
2024-11-14 11:10:42 +0600

    gallery fix try

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,13 +1,11 @@
-import BlogListComponent from "../components/blog/BlogList";
-
+import BlogList from "../components/blog/BlogList";
 import GalleryBody from "../components/galary/galleryBody";
 
 const GalleryPage = () => {
   return (
     <>
-      {/* <PageHeader title="Gallery" path="/" breadcrumbPath="Gallery" /> */}
       <GalleryBody />
-      <BlogListComponent />
+      <BlogList />
     </>
   );
 };
